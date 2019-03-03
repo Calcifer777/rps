2f83021a5142bcc224ee0ccb6f26ac4f593324af
calcifer <m.filip@hotmail.it>
2019-03-03 10:44:52 +0100

    Added test pkg_dir files

---
+++ pkg_dir/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rps_app',
+      version='1.0',
+      description='Rock Paper Scissors AI app',
+      author='Marco Filippone',
+      author_email='m.filip@hotmail.it',
+      # packages=['distutils', 'distutils.command'],
+     )
